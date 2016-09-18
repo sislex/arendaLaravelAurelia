021943b5dbc2ddc497b768788352486eafe9d039
Alex Rozhnov <alex.rozhnov@capsilon.com>
2016-09-18 15:53:08 +0300

    added aurelia

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -4,34 +4,7 @@
         <title>Добавил ларавел</title>
 
 
-        <style>
-            html, body {
-                height: 100%;
-            }
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
     </head>
     <body aurelia-app="main">
     <script src="scripts/vendor-bundle.js" data-main="aurelia-bootstrapper"></script>
