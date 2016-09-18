c4533d1e766147ad02914a205121a70a59e4c177
Alex Rozhnov <alex.rozhnov@capsilon.com>
2016-09-18 23:39:18 +0300

    added right routing for aurelia and laravel

--- public/src/app.ts
+++ public/src/app.ts
@@ -11,7 +11,8 @@ export class App {
     this.router.configure(config => {
       config.options.pushState = true;
       config.map([
-        {route: [''], moduleId: './Catalog/Index/Index', nav: true},
+        {route: [''], moduleId: './Catalog/Index/Index', name: 'home',  nav: true},
+        {route: ['logIn'], moduleId: './Catalog/Index/Index'},
       ]).mapUnknownRoutes({ redirect: '' });
     });
   }
